dcb797a9e9bd921681cdcf3b35df550d19d79067
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-03-31 13:27:55 +0900

    AI 추적 가능 거리 계산

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+
+        private void Update()
+        {
+            
+            if (DistanceToPlayer() < chaseDistance)
+            {
+                if (gameObject.name == "Enemy (1)") { return; }
+                print(gameObject.name + "추격");
+            }
+        }
+
+        private float DistanceToPlayer()
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            //플레이어와 대상의 거리 반환
+            return Vector3.Distance(player.transform.position, transform.position);
+        }
+    }
+}
+
+
