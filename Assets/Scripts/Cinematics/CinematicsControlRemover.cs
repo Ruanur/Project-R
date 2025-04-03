691879195e36085bbd06b87fa4da60431333ac00
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-04-03 12:31:13 +0900

    시네마틱 컨트롤 옵저버 패턴 테스트

---
+++ Assets/Scripts/Cinematics/CinematicsControlRemover.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicsControlRemover : MonoBehaviour
+    {
+        private void Start()
+        {
+            GetComponent<PlayableDirector>().played += DisableControl;
+            GetComponent<PlayableDirector>().stopped += EnableControl;
+        }
+
+        void DisableControl(PlayableDirector pd)
+        {
+            print("Disable");
+        }
+
+        void EnableControl(PlayableDirector pd)
+        {
+            print("Enable");
+        }
+    }
+}
