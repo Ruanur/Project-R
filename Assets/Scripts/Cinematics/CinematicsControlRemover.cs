26328d020fa484b5c4c588531f2958ef32f316d7
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-04-03 12:44:12 +0900

    컷 씬 등장 시 플레이어 컨트롤 비활성화 구현
    컷 씬 시작 시 진행중인 움직임 및 이동 명령 비활성화
    
    컷 씬 종료 시 컨트롤러 활성화

--- Assets/Scripts/Cinematics/CinematicsControlRemover.cs
+++ Assets/Scripts/Cinematics/CinematicsControlRemover.cs
@@ -1,24 +1,30 @@
-﻿using UnityEngine;
+﻿using RPG.Core;
+using RPG.Control;
+using UnityEngine;
 using UnityEngine.Playables;
 
 namespace RPG.Cinematics
 {
     public class CinematicsControlRemover : MonoBehaviour
     {
+        GameObject player;
+
         private void Start()
         {
             GetComponent<PlayableDirector>().played += DisableControl;
             GetComponent<PlayableDirector>().stopped += EnableControl;
+            player = GameObject.FindWithTag("Player");
         }
 
         void DisableControl(PlayableDirector pd)
         {
-            print("Disable");
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
         }
 
         void EnableControl(PlayableDirector pd)
         {
-            print("Enable");
+            player.GetComponent<PlayerController>().enabled = true;
         }
     }
 }
