6860e3b05d42af19e5b7483fc8a398becd767de5
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-04-04 14:03:12 +0900

    씬 로딩 포탈

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] int sceneToLoad = -1;
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                //씬 불러오기, LoadScene(String)의 경우 씬 이름을 바꿀 때 코드도 같이 바꿔야 하는 번거로움이 있어
+                //LoadScene(Index) 사용, Build Setting 설정으로 들어가 씬 인덱스 번호 확인 가능
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+    }
+}
