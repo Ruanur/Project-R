396293e1733c79107030f83b26a6b64175f39cf2
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-04-04 15:56:02 +0900

    포탈 간 연결 구현

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -8,8 +8,15 @@ namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifer
+        {
+            A, B, C, D, E
+        }
+
+
         [SerializeField] int sceneToLoad = -1;
         [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifer destination;
         private void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Player")
@@ -20,9 +27,13 @@ namespace RPG.SceneManagement
 
         private IEnumerator Transition()
         {
+            if (sceneToLoad < 0)
+            {
+                Debug.LogError("씬이 설정되지 않음");
+                yield break;
+            }    
             //씬 불러오기, LoadScene(String)의 경우 씬 이름을 바꿀 때 코드도 같이 바꿔야 하는 번거로움이 있어
             //LoadScene(Index) 사용, Build Setting 설정으로 들어가 씬 인덱스 번호 확인 가능
-            SceneManager.LoadScene(sceneToLoad);
 
             //씬 로드 전
             DontDestroyOnLoad(gameObject);
@@ -46,6 +57,7 @@ namespace RPG.SceneManagement
             foreach (Portal portal in FindObjectsOfType<Portal>())
             {
                 if (portal == this) continue;
+                if (portal.destination != destination) continue;
 
                 return portal;
             }
