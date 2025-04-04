6a6ae268418837e8256bb40792ca580c72ff2e81
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-04-04 15:07:37 +0900

    씬 로드 기다리기

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement
@@ -10,10 +11,23 @@ namespace RPG.SceneManagement
         {
             if (other.tag == "Player")
             {
-                //씬 불러오기, LoadScene(String)의 경우 씬 이름을 바꿀 때 코드도 같이 바꿔야 하는 번거로움이 있어
-                //LoadScene(Index) 사용, Build Setting 설정으로 들어가 씬 인덱스 번호 확인 가능
-                SceneManager.LoadScene(sceneToLoad);
+                StartCoroutine(Transition());
             }
+        }
+
+        private IEnumerator Transition()
+        {
+            //씬 불러오기, LoadScene(String)의 경우 씬 이름을 바꿀 때 코드도 같이 바꿔야 하는 번거로움이 있어
+            //LoadScene(Index) 사용, Build Setting 설정으로 들어가 씬 인덱스 번호 확인 가능
+            SceneManager.LoadScene(sceneToLoad);
+
+            //씬 로드 전
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            //씬 로드 후
+            print("Scene Loaded");
+            Destroy(gameObject);
         }
     }
 }
