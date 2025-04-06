6d438e0e947e951c4ca83a4e293f2f34c1fe905d
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-04-06 15:10:16 +0900

    Nested Coroutine을 이용한 페이드

---
+++ Assets/Scripts/SceneManagement/Fader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        CanvasGroup canvasGroup;
+
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+
+            StartCoroutine(FadeOutIn());
+        }
+
+        IEnumerator FadeOutIn()
+        {
+            yield return FadeOut(3f);
+            Debug.Log("페이드 아웃");
+            yield return FadeIn(1f);
+            Debug.Log("페이드 인");
+        }
+
+        public IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1) // Alpha != 1
+            {
+                canvasGroup.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+        }
+
+        public IEnumerator FadeIn(float time)
+        {
+            while (canvasGroup.alpha > 0) // Alpha != 1
+            {
+                canvasGroup.alpha -= Time.deltaTime / time;
+                yield return null;
+            }
+        }
+    }
+
+}
