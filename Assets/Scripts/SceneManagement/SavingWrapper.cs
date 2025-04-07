8c72562e27e5f1cc4b3f5a37a0f824b3b9266387
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-04-07 13:04:53 +0900

    저장 및 로드 트리거 구현

---
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,36 @@
+﻿using RPG.Saving;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string defaultSaveFile = "save";
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+        }
+
+        private void Save()
+        {
+            GetComponent<SavingSystem>().Save(defaultSaveFile);
+        }
+
+        private void Load()
+        {
+            //call to saving system load
+            GetComponent<SavingSystem>().Load(defaultSaveFile);
+        }
+    }
+}
