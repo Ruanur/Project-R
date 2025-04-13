307330784063d6d42e6f3f8332002cd08f93f1b1
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-04-13 15:39:04 +0900

    성장 시스템(레벨, 클래스) 디자인

---
+++ Assets/Scripts/Progression/BaseStats.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace RPG.Stats
+{
+    public class BaseStats : MonoBehaviour
+    {
+        [Range(1,99)]
+        [SerializeField] int startinglevel = 1;
+        [SerializeField] CharacterClass CharacterClass;
+
+        void Update()
+        {
+
+        }
+    }
+}
