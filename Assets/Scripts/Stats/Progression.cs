e3fcf032d3a68821f6601fca447ff88765e7003b
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-04-15 16:07:19 +0900

    딕셔너리를 사용해 최적화

--- Assets/Scripts/Stats/Progression.cs
+++ Assets/Scripts/Stats/Progression.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,25 +10,54 @@ namespace RPG.Stats
     {
         [SerializeField] ProgressionCharacterClass[] characterClasses = null;
 
+        Dictionary<CharacterClass, Dictionary<Stat, float[]>> lookupTable = null;
+
         public float GetStat(Stat stat, CharacterClass characterClass, int level)
         {
+            BuildLookup();
+
+            float[] levels = lookupTable[characterClass][stat];
+
+            if (levels.Length < level)
+            {
+                return 0;
+            }
+
+            return levels[level - 1];
+
+            //foreach (ProgressionCharacterClass progressionClass in characterClasses)
+            //{
+            //    if (progressionClass.characterClass != characterClass) continue;
+
+            //    foreach (ProgressionStat progressionStat in progressionClass.stats)
+            //    {
+            //        if (progressionStat.stat != stat) continue;
+
+            //        if (progressionStat.levels.Length < level) continue;
+
+            //        return progressionStat.levels[level - 1];
+            //    }
+            //}
+            //return 0;
+        }
+
+        private void BuildLookup()
+        {
+            if (lookupTable != null) { return; }
+
+            lookupTable = new Dictionary<CharacterClass, Dictionary<Stat, float[]>>();
+
             foreach (ProgressionCharacterClass progressionClass in characterClasses)
             {
-                if (progressionClass.characterClass != characterClass) continue;
+                var statLockupTable = new Dictionary<Stat, float[]>();
 
                 foreach (ProgressionStat progressionStat in progressionClass.stats)
                 {
-                    if (progressionStat.stat != stat) continue;
-
-                    if (progressionStat.levels.Length < level) continue;
-
-                    return progressionStat.levels[level - 1];
+                    statLockupTable[progressionStat.stat] = progressionStat.levels;
                 }
-                {
-                   //
-                }
+
+                lookupTable[progressionClass.characterClass] = statLockupTable;
             }
-            return 0;
         }
 
         [System.Serializable]
