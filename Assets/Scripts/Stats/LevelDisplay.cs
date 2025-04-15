c0f8023a0798a3ad42aa0934905385c93aa27f6d
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-04-15 16:55:08 +0900

    디버그 레벨 디스플레이

---
+++ Assets/Scripts/Stats/LevelDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RPG.Stats
+{
+    public class LevelDisplay : MonoBehaviour
+    {
+        BaseStats baseStats;
+        
+        private void Awake()
+        {
+            baseStats = GameObject.FindWithTag("Player").GetComponent<BaseStats>();
+        }
+
+        private void Update()
+        {
+            //체력 디버그 텍스트 갱신
+            //String.Format {0:0}, 소수점 표시 제거, 소수점 첫째 자리는 {0,0.0}
+            GetComponent<Text>().text = String.Format("{0:0}", baseStats.GetLevel());
+        }
+    }
+}
