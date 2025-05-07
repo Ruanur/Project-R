cded0ed88854d494e39305231c76cfe9aadff1cf
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-05-07 17:39:47 +0900

    comment

--- Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
+++ Assets/Scripts/UI/DamageText/DamageTextSpawner.cs
@@ -9,7 +9,7 @@ namespace RPG.UI.DamageText
         [SerializeField] DamageText damageTextPrefab = null;
 
 
-
+        //에디터에서 설정한 damageAmount값 출력 (일단은)
         public void Spawn(float damageAmount)
         {
             DamageText instance = Instantiate<DamageText>(damageTextPrefab, transform);
