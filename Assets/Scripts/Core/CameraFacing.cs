fd190d716909c45561547131eb1d1c251a957680
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-05-07 15:08:29 +0900

    Commit

--- Assets/Scripts/Core/CameraFacing.cs
+++ Assets/Scripts/Core/CameraFacing.cs
@@ -10,6 +10,7 @@ namespace RPG.Core
         // Update is called once per frame
         void Update()
         {
+            //Text가 카메라 기준으로 바라보게 
             transform.forward = Camera.main.transform.forward;
         }
     }
