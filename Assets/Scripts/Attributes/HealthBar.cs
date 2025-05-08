c77751491c0c93e14051519aacee4d3b5c9349c1
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-05-08 21:43:04 +0900

    체력바 비활성화
    체력이 0이거나 Max일 때 체력바 비활성화

--- Assets/Scripts/Attributes/HealthBar.cs
+++ Assets/Scripts/Attributes/HealthBar.cs
@@ -8,9 +8,18 @@ namespace RPG.Attributes
     {
         [SerializeField] Health healthComponent = null;
         [SerializeField] RectTransform foreground = null;
+        [SerializeField] Canvas rootCanvas = null;
 
         void Update()
         {
+            //Approximately : 근사값, 체력이 없거나 체력이 꽉찼을 떄, 체력바 비활성화
+            if (Mathf.Approximately(healthComponent.GetFraction(), 0) || Mathf.Approximately(healthComponent.GetFraction(), 1))
+            {
+                rootCanvas.enabled = false;
+                return;
+            }
+
+            rootCanvas.enabled = true;
             foreground.localScale = new Vector3(healthComponent.GetFraction(), 1, 1);
         }
     }
