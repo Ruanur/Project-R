8580b6b3c65ab2d98a973142419695768939f01f
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-05-09 15:19:06 +0900

    LateUpdate
    체력바 어긋남 개선

--- Assets/Scripts/Core/CameraFacing.cs
+++ Assets/Scripts/Core/CameraFacing.cs
@@ -7,8 +7,8 @@ namespace RPG.Core
     public class CameraFacing : MonoBehaviour
     {
 
-        // Update is called once per frame
-        void Update()
+        // Update -> LateUpdate, 체력바 어긋남 개선
+        void LateUpdate()
         {
             //Text가 카메라 기준으로 바라보게 
             transform.forward = Camera.main.transform.forward;
