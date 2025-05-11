99b346c9aeeac1d67cf4eab3bdf20061b8a4d760
Min Kyoung Jin <91407973+Ruanur@users.noreply.github.com>
2025-05-12 00:09:59 +0900

    사운드 위치 수정
    더 가까히서 들릴 수 있게 구현

--- Assets/Scripts/Combat/Weapon.cs
+++ Assets/Scripts/Combat/Weapon.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace RPG.Combat
 {
     public class Weapon : MonoBehaviour
     {
+        [SerializeField] UnityEvent onHit;
+
         public void OnHit()
         {
-            print("장착중인 무기 : " + gameObject.name);
+            onHit.Invoke();
         }
     }
 }
